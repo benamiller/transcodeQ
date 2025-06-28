f22c141f9a546de7635c3e22ccad5b6d549914e1
benamiller <mrbenamiller@gmail.com>
2025-06-28 12:30:22 +0000

    Simple bash for manual messing

---
+++ g/test.sh
@@ -0,0 +1,9 @@
+curl -X POST -H "Content-Type: application/json" -d '{"title": "My first video", "formats":["240p", "720p"]}' localhost:8080/jobs
+curl -X POST -H "Content-Type: application/json" -d '{"title": "My second video", "formats":["720p", "1080p"]}' localhost:8080/jobs
+curl -X POST -H "Content-Type: application/json" -d '{"title": "My third video", "formats":["240p","360p","720p","1080p"]}' localhost:8080/jobs
+curl -X POST -H "Content-Type: application/json" -d '{"title": "My fourth video", "formats":["240p","360p","480p","720p","1080p"]}' localhost:8080/jobs
+curl -X POST -H "Content-Type: application/json" -d '{"title": "My fifth video", "formats":["240p","360p","480p","720p","1080p"]}' localhost:8080/jobs
+curl -X POST -H "Content-Type: application/json" -d '{"title": "My sixth video", "formats":["240p","360p","480p","720p","1080p"]}' localhost:8080/jobs
+curl -X POST -H "Content-Type: application/json" -d '{"title": "My seventh video", "formats":["240p","360p","480p","720p","1080p"]}' localhost:8080/jobs
+curl -X POST -H "Content-Type: application/json" -d '{"title": "My eigth video", "formats":["240p","360p","480p","720p","1080p"]}' localhost:8080/jobs
+curl -X POST -H "Content-Type: application/json" -d '{"title": "My ninth video", "formats":["240p","360p","480p","720p","1080p"]}' localhost:8080/jobs
