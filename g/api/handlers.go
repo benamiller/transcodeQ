c954992ae3ac1d3dedaaeb8afb9c4b380a78a103
benamiller <mrbenamiller@gmail.com>
2025-06-28 07:36:54 +0000

    Populate id and format statuses server-side

--- g/api/handlers.go
+++ g/api/handlers.go
@@ -3,12 +3,23 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"sync"
 	"github.com/benamiller/transcodeQ/g/models"
 	"github.com/benamiller/transcodeQ/g/queue"
 )
 
 type API struct {
 	Queue *queue.JobQueue
+	mu	sync.Mutex
+	nextID int
+}
+
+func (api *API) NextID() {
+	api.mu.Lock()
+	defer api.mu.Unlock()
+
+	api.nextID++
+	return fmt.Sprintf("%d", api.nextID)
 }
 
 func (api *API) JobsHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,11 +40,24 @@ func (api *API) JobsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (api *API) CreateJobHandler(w http.ResponseWriter, r *http.Request) {
-	var job models.TranscodeJob
-	err := json.NewDecoder(r.Body).Decode(&job)
+	var req models.CreateTranscodeJobRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
+	}
+
+	newID := api.NextID()
+	newStatusMap := make(map[string]models.JobStatus)
+	for _, format := range req.formats {
+		newStatusMap[format] = models.StatusQueued
+	}
+
+	job := models.TranscodeJob {
+		ID: newID,
+		Title: req.title,
+		Formats: req.formats,
+		StatusMap: newStatusMap,
 	}
 
 	api.Queue.AddJob(job)
